16e8201f31ba8401c1c53e483012744d4e5c64db
OscarNaslundCuesta <81713281+OscarNaslundCuesta@users.noreply.github.com>
2023-06-19 18:46:35 +0200

    initial commit

---
+++ excercise01/task1.py
@@ -0,0 +1,34 @@
+from scipy import *
+from matplotlib.pyplot import *
+import sys
+import numpy
+
+"""
+print("----- Task 2 -----\n")
+
+x = 2.3
+equation = x ** 2 + 0.25 * x - 5
+print(equation)
+"""
+print("\n----- Task 6/8 -----\n")
+
+L1_task6 = []
+for i in range(100):
+    L1_task6.append(i)
+
+L2_task6 = [i / 99 for i in L1_task6]
+
+len(L2_task6)
+
+
+print("\n----- Task 9 -----\n")
+
+xplot = L2_task6
+yplot = [numpy.arctan(i) for i in xplot]
+
+
+print("\n----- Task 10 -----\n")
+
+plot(xplot, yplot)
+show()
+
